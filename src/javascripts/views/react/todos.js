5360cb4b13e4ee6b0bc9122e9e4d7f36c2c74090
takefumi-yoshii <takefumi.yoshii@dena.jp>
2017-07-27 16:49:26 +0900

    extract form component

--- src/javascripts/views/react/todos.js
+++ src/javascripts/views/react/todos.js
@@ -5,6 +5,7 @@ import { connectedProvider } from '~/lib/react/middleware'
 import { creators } from '~/redux/todos'
 import { TodoViewModel } from '~/models/todoView'
 import { TodoItem } from '~/views/react/todo'
+import { TodosForm } from '~/views/react/todosForm'
 import type { Store } from 'redux'
 
 export default function ReactTodos (store: Store<*, *>, dataset: Map<*, *>) {
@@ -15,47 +16,16 @@ export class TodosContainer extends Component {
   render () {
     return (
       <div className="todo-container">
-        <TodoForm { ...this.props } />
+        <TodosForm  { ...this.props } />
         <TodoItems { ...this.props } />
       </div>
     )
   }
 }
 
-function TodoForm ({ todos, pushTodo, updateInput, setPriority }: { todos: TodosViewModel, pushTodo: Function, updateInput: Function, setPriority: Function }) {
-  return (
-    <div className="navbar-default navbar-collapse collapse">
-      <form className="navbar-form navbar-right" onSubmit={ e => e.preventDefault() } >
-        <input className="form-control" type="text" onChange={ (e) => updateInput(e.target.value) } />
-        <button className="btn btn-primary active" onClick={ () => pushTodo() }>add todo</button>
-      </form>
-      <ul className="nav navbar-nav navbar-right">
-        <li className="dropdown">
-          <a role="button"
-            className="dropdown-toggle"
-            data-toggle="dropdown"
-            aria-expanded="false"
-            dangerouslySetInnerHTML={ todos.getPrioritySelectLabel() }
-          />
-          <PriorityDropdown options={ todos.getPriorityLabels() } setPriority={ setPriority } />
-        </li>
-      </ul>
-    </div>
-  )
-}
-
-function PriorityDropdown ({ options, setPriority }: { options: Array<string>, setPriority: Function }) {
-  const items = options.map((n, i) => <li key={i}><a onClick={ () => setPriority(i) }>{ n }</a></li>)
-  return (
-    <ul className="dropdown-menu" role="menu">
-      { items }
-    </ul>
-  )
-}
-
 function TodoItems (props: { todos: TodosViewModel, updateTodo: Function, deleteTodo: Function }) {
   const { todos, updateTodo, deleteTodo } = props
-  const list: Array<any> = todos.getTodosList()
+  const list: Array<any> = todos.getSortedTodosList()
   const items = list.map((item: TodoViewModel, i: number) => {
     const _props = { key: item.getID(), index: i, item, updateTodo, deleteTodo }
     return <TodoItem { ..._props } />
---
+++ src/javascripts/views/react/todosForm.js
@@ -0,0 +1,72 @@
+import { TodoViewModel } from '~/models/todoView'
+
+export function TodosForm ({ todos, pushTodo, updateInput, setPriority, setSortType }: { todos: TodosViewModel, pushTodo: Function, updateInput: Function, setPriority: Function, setSortType: Function }) {
+  return (
+    <div className="navbar-default navbar-collapse collapse">
+      <form className="navbar-form navbar-right" onSubmit={ e => e.preventDefault() } >
+        <input className="form-control" type="text" onChange={ (e) => updateInput(e.target.value) } />
+        <button className="btn btn-primary active" onClick={ () => pushTodo() }>add todo</button>
+      </form>
+      <SortDropdown todos={ todos } setSortType={ setSortType } />
+      <PriorityDropdown todos={ todos } setPriority={ setPriority } />
+    </div>
+  )
+}
+// _____________________________________________
+
+function SortDropdown ({ todos, setSortType }: { todos: TodosViewModel, setSortType: Function }) {
+  return (
+    <ul className="nav navbar-nav navbar-left">
+      <li className="dropdown">
+        <a role="button"
+          className="dropdown-toggle"
+          data-toggle="dropdown"
+          aria-expanded="false"
+          dangerouslySetInnerHTML={ todos.getSortTypeSelectLabel() }
+        />
+        <SortDropdownMenu
+          options={ todos.getSortTypeLabels() }
+          setSortType={ setSortType }
+        />
+      </li>
+    </ul>
+  )
+}
+
+function SortDropdownMenu ({ options, setSortType }: { options: Array<string>, setSortType: Function }) {
+  const items = options.map((n, i) => <li key={i}><a onClick={ () => setSortType(i) }>{ n }</a></li>)
+  return (
+    <ul className="dropdown-menu" role="menu">
+      { items }
+    </ul>
+  )
+}
+// _____________________________________________
+
+function PriorityDropdown ({ todos, setPriority }: { todos: TodosViewModel, setPriority: Function }) {
+  return (
+    <ul className="nav navbar-nav navbar-right">
+      <li className="dropdown">
+        <a role="button"
+          className="dropdown-toggle"
+          data-toggle="dropdown"
+          aria-expanded="false"
+          dangerouslySetInnerHTML={ todos.getPrioritySelectLabel() }
+        />
+        <PriorityDropdownMenu
+          options={ todos.getPriorityLabels() }
+          setPriority={ setPriority }
+        />
+      </li>
+    </ul>
+  )
+}
+
+function PriorityDropdownMenu ({ options, setPriority }: { options: Array<string>, setPriority: Function }) {
+  const items = options.map((n, i) => <li key={i}><a onClick={ () => setPriority(i) }>{ n }</a></li>)
+  return (
+    <ul className="dropdown-menu" role="menu">
+      { items }
+    </ul>
+  )
+}
