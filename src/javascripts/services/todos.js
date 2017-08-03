e54edc717f3ff78f7d62cf33740b08fb5f91d54a
takefumi-yoshii <takefumi.yoshii@dena.jp>
2017-08-03 15:32:56 +0900

    add context mapping

--- src/javascripts/services/todos.js
+++ src/javascripts/services/todos.js
@@ -1,4 +1,38 @@
-function * task () {
+import { take, put } from 'redux-saga/effects'
+import { types } from '~/redux/todosForm'
+import { creators } from '~/redux/todos'
+
+function * subscribeSubmit () {
+  while (true) {
+    const { payload } = yield take(types.submitForm)
+    yield put(creators.pushTodo(payload))
+  }
 }
 
-export const todosSaga = [task]
+function * subscribeSetPriority () {
+  while (true) {
+    const { payload } = yield take(types.setPriority)
+    yield put(creators.setPriority(payload))
+  }
+}
+
+function * subscribeSetSortType () {
+  while (true) {
+    const { payload } = yield take(types.setSortType)
+    yield put(creators.setSortType(payload))
+  }
+}
+
+function * subscribeSetOrder () {
+  while (true) {
+    const { payload } = yield take(types.setOrder)
+    yield put(creators.setOrder(payload))
+  }
+}
+
+export const todosSaga = [
+  subscribeSubmit,
+  subscribeSetPriority,
+  subscribeSetSortType,
+  subscribeSetOrder
+]
